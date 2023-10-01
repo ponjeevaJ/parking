e48f48c29904f8aa4c7b044456a6572c28335b34
ponjeevaJ <ponjeevajeeva2002@gmail.com>
2023-10-01 21:20:03 +0530

    new

---
+++ login.js
@@ -0,0 +1,13 @@
+document.getElementById('log').onclick = clk;
+function clk() {
+    window.open("login.html")
+  }
+  document.getElementById('cre').onclick = clk1;
+function clk1() {
+    window.open("create.html")
+  }
+
+    // document.querySelector("#log").addEventListener('click',function(e){
+    //   e.preventDefault();
+    //   window.location.href = "/login.html";
+    // })
